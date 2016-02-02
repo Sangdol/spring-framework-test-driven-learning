bb832069ffac93ba17a9274080c805c756f46065
Hugh Lee <hammerha@gmail.com>
2016-02-02 16:00:15 +0900

    Use @RestController

--- src/main/java/com/sangdol/springSimple/HomeController.java
+++ src/main/java/com/sangdol/springSimple/HomeController.java
@@ -1,32 +1,24 @@
 package com.sangdol.springSimple;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
  * @author hugh
  */
-@Controller
+@RestController
 @RequestMapping("home")
 public class HomeController {
 
     @RequestMapping
-    @ResponseBody
     public String helloWorld(String name) {
         return "Hello " + name + "!";
     }
 
     @RequestMapping("map")
-    @ResponseBody
     public Map<String, String> helloMap(String name) {
         return ImmutableMap.of("Hello", name);
     }
