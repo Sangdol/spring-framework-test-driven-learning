6f0ff0945bcf6f4b15e111f6fad055fbb218efd1
Hugh Lee <hammerha@gmail.com>
2016-02-11 18:38:05 +0900

    Add SpEL test

--- src/test/java/SpelTest.java
+++ src/test/java/SpelTest.java
@@ -1,6 +1,23 @@
+import org.junit.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * http://docs.spring.io/spring/docs/current/spring-framework-reference/html/expressions.html
  * @author hugh
  */
 public class SpelTest {
+
+    @Test
+    public void parser() throws Exception {
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression("'Hello World'");
+        assertThat(exp.getValue(), is("Hello World"));
+    }
+
+
 }
